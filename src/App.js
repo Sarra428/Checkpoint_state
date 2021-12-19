f3c526696a2f12445f010c01ed202e36dbcb1f8d
sarra <sarrah.aouina@gmail.com>
2021-12-19 20:57:06 +0100

    checkpoint state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+
+export class App extends Component {
+
+
+  state={
+    FullName: "Sarra Aouina",
+    bio: "Full Stack Developer",
+    profession: "Student",
+    show: true,
+    img: <img  src="https://ds1.static.rtbf.be/article/image/1920xAuto/2/a/0/5e62d03aec0d17facfc5355dd90d441c-1512575731.png"   className='pic' />,
+  }
+
+
+
+  
+
+  toggleshow=()=>{this.setState({show:!this.state.show  } ) }
+
+
+
+
+  render() {
+    return (
+      <div>
+        <button  onClick={()=>this.toggleshow()}>    
+        {this.state.show ? 'Show'   :"Back" }
+        </button>
+       {this.state.show!==true  ?  <div>
+         
+         <ul>
+        <div className='profile'> {this.state.img}</div>
+       <div className='mypro'> {this.state.FullName} <br/>
+          {this.state.bio} <br/>
+          {this.state.profession}
+         </div>
+         
+          </ul>
+         </div> 
+                 :null }
+      
+
+      </div>
+    )
+  }
 }
+
+
+
 
 export default App;
